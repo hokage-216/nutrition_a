55b28678b77cfbd12684006124573b7f8cbf24b6
mcook2323 <mattcook2323@gmail.com>
2024-03-02 09:02:57 -0500

    Added Meal Plan Model

---
+++ models/DailyMealPlan.js
@@ -0,0 +1,62 @@
+
+
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class DailyMealPlan extends Model {}
+
+DailyMealPlan.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    date: {
+      type: DataTypes.DATEONLY,
+      allowNull: false,
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'User', 
+        key: 'id',
+      },
+    },
+    breakfastId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'Meal',
+        key: 'id',
+      },
+    },
+    lunchId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'Meal',
+        key: 'id',
+      },
+    },
+    dinnerId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'Meal',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    underscored: true,
+    modelName: 'daily_meal_plan',
+    tableName: 'daily_meal_plans',
+  }
+);
+
+module.exports = DailyMealPlan;

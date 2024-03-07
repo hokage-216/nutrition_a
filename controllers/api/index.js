7656e2e7c40ca4b1a69d87aca61d7d118b1ae5c3
mcook2323 <mattcook2323@gmail.com>
2024-03-06 21:35:35 -0500

    Updated fetch, planner.js, and meal plan handlebar

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const plannerRoutes = requrie('./plannerRoutes');
+const plannerRoutes = require('./plannerRoutes');
 const nutritionRoutes = require('./nutritionsRoutes');
 
 router.use('/users', userRoutes);

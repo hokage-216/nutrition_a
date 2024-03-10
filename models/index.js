dc664c5e6e897e376fd6725e0e32244559e3dc24
LeVente Berry <berrylevente@gmail.com>
2024-03-10 11:33:09 -0400

    updated routs to work properly

--- models/index.js
+++ models/index.js
@@ -2,47 +2,12 @@ const Sequelize = require('sequelize');
 const sequelize = require('../config/connection');
 
 
-const UserModel = require('./User');
-const MealModel = require('./Meal');
+const User = require('./User');
+const Meal = require('./Meal');
 
 
-const User = new UserModel(sequelize, Sequelize);
-const Meal = new  MealModel(sequelize, Sequelize);
+User.hasMany(Meal, {
+  foreignKey: 'mealId', 
+});
 
-
-// User.hasMany(DailyMealPlan, {
-//   foreignKey: 'userId', 
-// });
-
-// DailyMealPlan.belongsTo(User, {
-//   foreignKey: 'userId', 
-// });
-
-// DailyMealPlan.belongsTo(Meal, {
-//   foreignKey: 'breakfastId', 
-//   as: 'breakfast' 
-// });
-
-// DailyMealPlan.belongsTo(Meal, {
-//   foreignKey: 'lunchId', 
-//   as: 'lunch' 
-// });
-
-// DailyMealPlan.belongsTo(Meal, {
-//   foreignKey: 'dinnerId', 
-//   as: 'dinner' 
-// });
-
-// Meal.hasOne(Recipe, {
-//   foreignKey: 'mealId', 
-// });
-
-// Recipe.belongsTo(Meal, {
-//   foreignKey: 'mealId', 
-// });
-
-
-module.exports = {
-  User,
-  Meal,
-};
+module.exports = { User, Meal };

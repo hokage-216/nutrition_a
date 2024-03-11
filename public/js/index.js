e4727d783fa85dda6bd6d8ed52ac0d5fdd617f84
mcook2323 <mattcook2323@gmail.com>
2024-03-11 13:39:45 -0400

    udpate

---
+++ public/js/index.js
@@ -0,0 +1,12 @@
+// recipesmodule
+
+class Recipe {
+  constructor(title, ingredients, instructions) {
+    this.title = title;
+    this.ingredients = ingredients;
+    this.instructions = instructions;
+  }
+
+}
+
+module.exports = Recipe;

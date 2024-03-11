e111b69098829daee1d20d4cf36e89ff89b94660
Josh <joshschulze05@gmail.com>
2024-03-11 10:03:41 -0400

    changes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,7 +2,10 @@ const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const plannerRoutes = require('./plannerRoutes');
 const nutritionRoutes = require('./nutritionRoutes');
+const controller = require('./controller');
 
+router.get('/recipes/search/:query', controller);
+router.use('/search', controller);
 router.use('/users', userRoutes);
 router.use('/planner', plannerRoutes);
 router.use('/nutrition', nutritionRoutes);

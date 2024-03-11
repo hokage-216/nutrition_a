170fceb0e2c128e50b108de2a295ffc60e33491c
mcook2323 <mattcook2323@gmail.com>
2024-03-11 13:21:40 -0400

    updates

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,9 +2,13 @@ const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const plannerRoutes = require('./plannerRoutes');
 const nutritionRoutes = require('./nutritionRoutes');
+const controller = require('./controller');
+const finderRoutes = require('./finderRoutes')
 
+router.get('/recipes/search/:query', controller);
+router.use('/search', controller);
 router.use('/users', userRoutes);
 router.use('/planner', plannerRoutes);
-router.use('/nutrition', nutritionRoutes);
+router.use('/finder', finderRoutes);
 
 module.exports = router;
